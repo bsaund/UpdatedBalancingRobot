6c15ba0e84df7a1416a4df725cb7661213ce142d
Brad Saund <brad.saund@gmail.com>
2016-09-09 01:27:46 -0400

    Visualizing robot frame in RViz

---
+++ examples/ros/src/balancing_robot/src/arduinoParsing.cpp
@@ -0,0 +1,43 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
+#include <math.h>
+
+ros::Subscriber ucListener;
+
+
+void ucListenerCallback(const std_msgs::String::ConstPtr& msg) {
+
+  double theta = atof(msg->data.c_str())*M_PI/180;
+  
+  static tf::TransformBroadcaster transformBroadcaster;
+  tf::Vector3 v(0,0,0);
+  tf::Quaternion q;
+  q.setEuler(0,theta,0);
+  tf::Transform trans(q,
+		      tf::Vector3(0,0,0));
+		      // v);
+  tf::StampedTransform transStmp(trans, ros::Time::now(),
+				"base_frame", "robot_frame");
+  transformBroadcaster.sendTransform(transStmp);
+							  
+}
+  
+
+int main(int argc, char **argv){
+
+  ros::init(argc, argv, "arduinoDataParsing");
+  ros::NodeHandle rosNode;
+  ROS_INFO("arduinoDataParsing starting");
+
+
+
+  ucListener = rosNode.subscribe("/uc0Response", 100, ucListenerCallback);
+  // transformBroadcaster = tf::TransformBroadcaster();
+  
+  ros::spin();
+
+  ROS_INFO("arduinoParsing stopping");
+  return 0;
+}
