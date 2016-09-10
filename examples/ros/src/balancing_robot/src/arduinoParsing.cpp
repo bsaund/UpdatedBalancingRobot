c40a4f492d83a0a64a91cbd837e1e9ab8989fe99
Brad Saund <brad.saund@gmail.com>
2016-09-10 12:43:56 -0400

    Robot visualization translates with wheel rotation

--- examples/ros/src/balancing_robot/src/arduinoParsing.cpp
+++ examples/ros/src/balancing_robot/src/arduinoParsing.cpp
@@ -3,26 +3,60 @@
 #include <tf/tf.h>
 #include <tf/transform_broadcaster.h>
 #include <math.h>
+#include <sstream>
 
 ros::Subscriber ucListener;
+long lEnc, rEnc;
+tf::Transform unrotTrans;
+double wheelRadius = 0.033;
+double wheelGap = .17;
+double prevLEnc=0, prevREnc=0;
 
 
-void ucListenerCallback(const std_msgs::String::ConstPtr& msg) {
-
-  double theta = atof(msg->data.c_str())*M_PI/180;
-  double wheelRadius = 0.033;
-  
-  static tf::TransformBroadcaster transformBroadcaster;
+tf::StampedTransform makeRotatedMsg(double theta){
   tf::Vector3 v(0,-theta*wheelRadius,wheelRadius);
   tf::Quaternion q;
   q.setEuler(0,theta,0);
   tf::Transform trans(q,v);
 
-  tf::StampedTransform transStmp(trans, ros::Time::now(),
-				"base_frame", "robot_frame");
-  transformBroadcaster.sendTransform(transStmp);
+  return tf::StampedTransform(trans, ros::Time::now(),
+			      "robot_unrotated_frame", "robot_frame");
+}
+
+tf::StampedTransform makeUnrotatedMsg(double dL, double dR){
+  tf::Vector3 v(0,(dL+dR)*wheelRadius/2, 0);
+  tf::Quaternion q;
+  q.setEuler(0,0,(dL-dR)*wheelRadius/wheelGap);
+
+  tf::Transform delta(q, v);
+  unrotTrans = unrotTrans * delta;
+  return tf::StampedTransform(unrotTrans, ros::Time::now(),
+			      "base_frame", "robot_unrotated_frame");
+
+}
+
+
+void ucListenerCallback(const std_msgs::String::ConstPtr& msg) {
+
+  std::stringstream stream(msg->data.c_str());
+  double theta, lEncNew, rEncNew;
+
+  stream >> theta;
+  stream >> lEncNew;
+  stream >> rEncNew;
+
+  double dL = lEncNew - prevLEnc;
+  double dR = rEncNew - prevREnc;
+  prevLEnc = lEncNew;
+  prevREnc = rEncNew;
+  
+  static tf::TransformBroadcaster tfBroad;
+  tfBroad.sendTransform(makeRotatedMsg(theta*M_PI/180));
+  tfBroad.sendTransform(makeUnrotatedMsg(dL, dR));
+
 							  
 }
+
   
 
 int main(int argc, char **argv){
@@ -31,10 +65,10 @@ int main(int argc, char **argv){
   ros::NodeHandle rosNode;
   ROS_INFO("arduinoDataParsing starting");
 
-
+  unrotTrans.setIdentity();
 
   ucListener = rosNode.subscribe("/uc0Response", 100, ucListenerCallback);
-  // transformBroadcaster = tf::TransformBroadcaster();
+
   
   ros::spin();
 
