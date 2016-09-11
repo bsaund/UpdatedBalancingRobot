8066356fce4edfcbb7e799e5f2c4fc0dcfb335b1
Brad Saund <brad.saund@gmail.com>
2016-09-11 01:38:08 -0400

    Publish mode and angular velocity to arduino

--- examples/ros/src/balancing_robot/scripts/arduinoCommand.py
+++ examples/ros/src/balancing_robot/scripts/arduinoCommand.py
@@ -5,12 +5,16 @@ import rospy
 from geometry_msgs.msg import Twist
 from std_msgs.msg import String
 
-pub = rospy.Publisher('uc0Command', String, queue_size = 1);
+pub = rospy.Publisher('uc0Command', String, queue_size = 6);
 
 def directControlCallback(msg):
     xval = msg.linear.x;
     if(abs(xval)>0.01):
-        str = "x %f%s" % (msg.linear.x, ";")
+        str = "vx %f%s" % (msg.linear.x/50, ";")
+        pub.publish(str)
+        str = "vw %f%s" % (msg.angular.z, ";")
+        pub.publish(str)
+        str = "md   ;"
         pub.publish(str)
 
 def main():
